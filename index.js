bc491367a8bd161802e35ed24a62a6998dacdc45
ADotDong <amydong7777@gmail.com>
2018-08-08 16:07:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
 const currentUser = "Anonymous";
-const welcomeMessage = "Welcome to Klossybook, "+ currentUser + "!";
+const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
