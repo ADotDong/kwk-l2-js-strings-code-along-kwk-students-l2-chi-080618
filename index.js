9619ca0f59da2396e1ecc9d69cfa21a34728db29
ADotDong <amydong7777@gmail.com>
2018-08-08 16:01:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 const currentUser = "Anonymous";
 const welcomeMessage = "Welcome to Klossybook, "+ currentUser + "!";
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = "Welcome"
+const shortGreeting = "Welcome, "
